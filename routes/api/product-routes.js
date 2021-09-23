b7084a928091ef6a5b6935c88e2c6646c9a5a0ff
David Tierney <daveshouse44@hotmail.com>
2021-09-23 01:43:39 -0500

    Refactored one my async await, still trying to delete products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -12,11 +12,10 @@ router.get("/", async (req, res) => {
       include: [
         {
           model: Category,
-          attributes: ["category_name"],
         },
         {
           model: Tag,
-          attributes: ["id", "tag_name"],
+          through: ProductTag,
         },
       ],
     });
@@ -31,36 +30,31 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Category,
-        attributes: ["category_name"],
-      },
-      {
-        model: Tag,
-        attributes: ["tag_name"],
-      },
-    ],
-  })
-    .then((productData) => {
-      if (!productData) {
-        res.status(404).json({ message: "No Products Found" });
-        return;
-      }
-      res.json(productData);
-      //console.log(productData)
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
     });
+    if (!productData) {
+      res.status(404).json({ message: "No Products Found" });
+      return;
+    }
+    res.status(200).json(productData);
+    //console.log(productData)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
