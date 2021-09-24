245deaff1098534f50705777dfa135707164006f
David Tierney <daveshouse44@hotmail.com>
2021-09-24 16:34:15 -0500

    Updated README and fixed tag-routes.js

--- README.md
+++ README.md
@@ -44,7 +44,6 @@ The following animation shows the application's POST, PUT, and DELETE routes for
 
 ![In Insomnia Core, the user tests “DELETE Category by ID,” “CREATE Category,” and “UPDATE Category.”](./Assets/13-orm-homework-demo-03.gif)
 
-Your walkthrough video should also show the POST, PUT, and DELETE routes for products and tags being tested in Insomnia Core.
 
 ## Install:
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,10 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
+    const tagData = await Tag.create({
+      tag_name: req.body.tagData,
+    });
+    res.status(200).json({ message: "Tag created" });
   } catch (err) {
     res.status(400).json(err);
   }
@@ -51,22 +53,16 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(
-      {
-        tag_name: req.body.tag_name,
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
       },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
+    });
     if (!tagData) {
       res.status(404).json({ message: "No tag found with that ID" });
       return;
     }
-
-    res.status(200).json(tagData);
+    res.status(200).json({ message: "Tag updated" });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -82,11 +78,11 @@ router.delete("/:id", async (req, res) => {
     });
 
     if (!tagData) {
-      res.status(404).json({ message: "No tag found with this id!" });
+      res.status(404).json({ message: "No tag found with this id" });
       return;
     }
 
-    res.status(200).json(tagData);
+    res.status(200).json({ message: "Tag deleted" });
   } catch (err) {
     res.status(500).json(err);
   }
